092912e3b915df8340177705cf33322cf12238c1
lukak <lukak@thevtool.com>
2024-03-01 14:26:32 +0100

    Add skeleton for changing file contents

--- generate-app-structure.sh
+++ generate-app-structure.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Check if host system is debian-based
+echo "Checking system configuration..."
 if [ -f /etc/debian_version ]; then
-	echo "Debian-based system detected..."
 	echo
 else
 	echo "This script is intended for Debian-based systems only. Exiting..."
@@ -30,13 +30,15 @@ is_dependency_installed() {
 }
 
 # Install sw that is missing on the system
+echo "Checking software prerequisites..."
 for dep in "${dependencies[@]}"; do
 	if ! is_dependency_installed "$dep"; then
 		echo "Installing $dep..."
 		sudo apt install -y "$dep"
 		echo
 	else
-		echo "$dep is already installed."
+		# echo "$dep is already installed."
+		continue
 	fi
 done
 
@@ -45,11 +47,68 @@ echo
 echo "Generating UUID..."
 uuid=$(uuidgen)
 
-# Clone repo with template app "my_example"
-echo
-git clone https://github.com/l-krstic/optee-template-app.git
-
 # Ask user for application name
 echo
 echo "Enter application name:"
 read app_name
+
+# Clone repo with template app "my_example"
+echo
+git clone https://github.com/l-krstic/optee-template-app.git
+cd optee-template-app
+
+# Rename application directory
+echo
+echo "Renaming my_example directory name to $app_name..."
+mv my_example/ $app_name
+
+# Save UUID in file
+echo
+echo "Saving uuid in ${app_name}_UUID.txt..."
+touch ${app_name}_UUID.txt
+echo $uuid > ${app_name}_UUID.txt
+
+# Making changes in application files
+echo
+echo "------- Making host section changes"
+cd ${app_name}/host/
+echo "host/Makefile..."
+#...
+echo "host/main.c..."
+#...
+echo "host/main.c..."
+#...
+echo "host/main.c..."
+#...
+
+echo
+echo "------- Making trusted section changes"
+cd ../ta
+echo "ta/Android.mk..."
+#...
+echo "ta/user_ta_header_defines.h..."
+#...
+echo "ta/user_ta_header_defines.h..."
+#...
+echo "ta/user_ta_header_defines.h..."
+#...
+echo "ta/sub.mk..."
+#...
+echo "ta/Makefile..."
+#...
+echo "ta/${app_name}_ta.c..."
+#...
+echo "ta/${app_name}_ta.c..."
+#...
+echo "ta/include/${app_name}_ta.h..."
+#...
+echo "ta/include/${app_name}_ta.h..."
+#...
+
+echo
+echo "------- Making high-level build helper changes"
+cd ..
+echo "CMakeLists.txt..."
+#...
+echo "Android.mk..."
+#...
